190bb7576daf6be08c35f040ccc4961886b7f123
Matthias Hillebrand <cto@ta-management.de>
2024-07-31 13:36:18 +0200

    first version of uploads

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,83 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import { supabase } from './lib/supabaseClient'
+
+let photos = ref([])
+let loading = ref(false)
+
+const uploadFile = async (event) => {
+  console.log(event)
+  loading.value = true
+  for (var i = 0; i < event.target.files.length; i++) {
+    const file = event.target.files[i]
+    const { data, error } = await supabase
+      .storage
+      .from('photos_public')
+      .upload(file.name, file)
+      const url = supabase.storage.from('photos_public').getPublicUrl(file.name)
+      data.url = url.data.publicUrl
+    if (error) {
+      console.error(error)
+    } else {
+      photos.value.push(data)
+    }
+  }
+  await listFiles()
+  loading.value = false
+}
+
+const listFiles = async () => {
+  const { data, error } = await supabase
+    .storage
+    .from('photos_public')
+    .list()
+  if (error) {
+    console.error(error)
+  } else {
+    data.forEach(photo => {
+      const url = supabase.storage.from('photos_public').getPublicUrl(photo.name, {download:true})
+      photo.url = url.data.publicUrl
+    })
+    photos.value = data
+  }
+  console.log(photos.value)
+}
+
+onMounted(async () => {
+  loading.value = true
+  await listFiles()
+  loading.value = false
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-col align="center">
+    <v-card class="mx-4">
+      <v-card-title class="bg-indigo">Hochzeitsbilder</v-card-title>
+      <v-card-text class="mt-4">
+        <v-file-input chips variant="outlined" label="Fotos hochladen" @change="uploadFile" multiple></v-file-input>
+      </v-card-text>
+    </v-card>
+  </v-col>
+  <v-progress-linear v-if="loading" indeterminate color="indigo"></v-progress-linear>
+    
+    <v-row align="center" justify="center" class="mx-4">
+      
+      <template v-for="photo in photos" :key="photo.id" >
+      <v-col v-if="photo.name != '.emptyFolderPlaceholder'" cols="4" align="center" class="d-flex child-flex">
+        <v-img
+            :lazy-src="photo.url"
+            :src="photo.url"
+            aspect-ratio="1"
+            class="bg-grey-lighten-2"
+            cover>
+        </v-img>
+      </v-col>
+    </template>
+    </v-row>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://pfdjhezomxoegwalfgca.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmZGpoZXpvbXhvZWd3YWxmZ2NhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI1ODA0MjAsImV4cCI6MTk5ODE1NjQyMH0.LRAXR-Iel4DdFLuO5bGVvNCNwLvNM8Lmf_eZyYSOWSc')
