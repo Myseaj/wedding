0d1a108efe748d79a6516cab24b6700026eaae1c
Matthias Hillebrand <cto@ta-management.de>
2024-08-05 17:45:11 +0200

    new version

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,74 @@
+<template>
+  <v-row align="center" justify="center" class="my-4">
+    <v-col align="center" cols="12">
+      <v-card flat class="mb-2">
+        <v-row justify="center">
+          <v-col justify="center">
+            <v-img height="80" cover gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)" src="https://png.pngtree.com/thumb_back/fh260/background/20230408/pngtree-happy-wedding-background-image_2138394.jpg">
+              <div class="pa-2 wedding-title text-white">Hochzeitsfotos & Photoquest Joanna & Matthias</div></v-img>
+          </v-col>
+        </v-row>
+      </v-card>
+      
+      <v-card class="mb-4" elevation="5">
+        <v-card-title class="bg-indigo">Aufgaben</v-card-title>
+        <v-card-text class="ma-4">
+          <div class="text-caption">Mach bei unserer Photoquest mit oder lade Bilder einfach so hoch!</div>
+          <template v-for="task in tasks">
+            <li style="font-size:6pt" v-if="task.id > 0">{{ task.task }}</li>
+          </template>
+        </v-card-text>
+      </v-card>
+      <v-card class="mb-4" elevation="5">
+        <v-card-title class="bg-indigo">Bilder hochladen</v-card-title>
+        <v-card-text class="mt-4">
+          <v-row align="center" justify="center">
+            <v-col cols="12">
+              <v-select density="compact" v-model="selectedTask" item-title="short"  item-value="short" :items="tasks" label="Aufgabe" variant="outlined"></v-select>
+              <v-text-field density="compact" variant="outlined" v-model="uploader" label="Dein Name"></v-text-field>
+              <v-file-input prepend-icon="mdi-camera" density="compact" v-model="fileInput" chips variant="outlined" label="Fotos hochladen" @change="uploadFile" multiple></v-file-input>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+      <v-card  :loading="loading"  elevation="5">
+        <v-card-title class="bg-indigo mb-4">Galerie</v-card-title>
+        <v-card-text>
+          <v-col>
+            <v-pagination @click="showFiles(photoObject.page)" size="small" v-model="photoObject.page" :length="photoObject.length" density="compact"></v-pagination>
+          </v-col>
+          
+          <v-row align="center" justify="center">
+            <v-col cols="12" v-if="loading" >
+              <v-progress-linear indeterminate color="indigo" height="30"></v-progress-linear>
+            </v-col>
+            <template v-for="photo in photos" :key="photo.id" >
+              
+              <v-col v-if="photo.name != '.emptyFolderPlaceholder'" cols="4" align="center" class="d-flex child-flex">
+                <v-card flat style="font-size:5pt">
+                  {{ photo.uploader ? photo.uploader : "Unbekannt" }}: {{ photo.task ? photo.task : "Allgemein" }}
+                  <v-img
+                    style="cursor: pointer"
+                    @click="openFile(photo)"
+                    :lazy-src="photo.public_url"
+                    :src="photo.public_url"
+                    aspect-ratio="1"
+                    class="bg-grey-lighten-2"
+                    cover>
+                  </v-img>
+                </v-card>
+              </v-col>
+          </template>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  
+    
+    
+  </v-row>
+</template>
+
 <script setup>
 import { ref, onMounted } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
@@ -7,12 +78,27 @@ let photos = ref([])
 let loading = ref(false)
 let fileInput = ref(null)
 
+const tasks = [
+  {id:"0",short:"Allgemein",task:"Allgemein"},
+  {id:"1",short:"Foto mit allen vom Tisch",task:"Mach ein Foto mit allen Personen an deinem Tisch"},
+  {id:"2",short:"Fotos mit Bekannten",task:"Mach ein Foto mit allen Gästen, die du schon vor der Hochzeit kanntest"},
+  {id:"3",short:"Foto mit der ähnlichsten Person",task:"Mach ein Bild mit der Person auf der Hochzeit, die dir am ähnlichsten sieht"},
+  {id:"4",short:"Foto mit ältester Person",task:"Mach ein Foto mit dem ältesten Gast auf der Hochzeit."},
+  {id:"5",short:"Foto mit jüngster Person",task:"Mach ein Foto mit dem jüngsten Gast auf der Hochzeit."},
+  {id:"6",short:"Foto mit weitgereister Person",task:"Mach ein Foto mit dem Brautpaar und einem Gast, der am weitesten gereist ist."},
+  {id:"7",short:"Foto mit Hochzeitsdebütant",task:"Finde einen Gast, der noch nie auf einer Hochzeit war, und macht ein Foto zusammen."},
+  {id:"8",short:"Foto mit gleichen Hobbys",task:"Mach ein Foto mit einem Gast, der das gleiche Hobby wie du hat."},
+]
+
 let photoObject = ref({
   length: 1,
   page: 1,
   photosPerPage: 6,
-  photoItems:[]
+  photoItems:[],
 })
+
+let selectedTask = ref(tasks[0].short)
+let uploader = ref(null)
 
 const uploadFile = async (event) => {
   loading.value = true
@@ -27,10 +113,12 @@ const uploadFile = async (event) => {
       if (error) {
         console.error(error)
       }
+
       await supabase
       .from('photos')
       .insert({
-        uploader: null,
+        uploader: uploader.value,
+        task: selectedTask.value,
         name: file.name,
         public_url: url.data.publicUrl
       })
@@ -94,58 +182,6 @@ onMounted(async () => {
 })
 
 </script>
-
-<template>
-  <v-row align="center" justify="center" class="my-4">
-    <v-col align="center" cols="12">
-      <v-card flat class="mb-2">
-        <div class="wedding-title">Hochzeitsfotos Joanna & Matthias</div>
-      </v-card>
-      <v-card class="mx-4" elevation="5">
-        <v-card-title class="bg-indigo">Bilder hochladen</v-card-title>
-        <v-card-text class="mt-4">
-          <v-row align="center" justify="center">
-            <v-col cols="12" align="center" class="mr-10">
-              <v-file-input v-model="fileInput" chips variant="outlined" label="Fotos hochladen" @change="uploadFile" multiple></v-file-input>
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
-    </v-col>
-    <v-col align="center" cols="12">
-      <v-card class="mx-4" :loading="loading"  elevation="5">
-        <v-card-title class="bg-indigo mb-4">Galerie</v-card-title>
-        <v-card-text>
-          <v-col>
-            <v-pagination @click="showFiles(photoObject.page)" size="small" v-model="photoObject.page" :length="photoObject.length" density="compact"></v-pagination>
-          </v-col>
-          
-          <v-row align="center" justify="center">
-            <v-col cols="12" v-if="loading" >
-              <v-progress-linear indeterminate color="indigo" height="30"></v-progress-linear>
-            </v-col>
-            <template v-for="photo in photos" :key="photo.id" >
-            <v-col v-if="photo.name != '.emptyFolderPlaceholder'" cols="4" align="center" class="d-flex child-flex">
-              <v-img
-                  style="cursor: pointer"
-                  @click="openFile(photo)"
-                  :lazy-src="photo.public_url"
-                  :src="photo.public_url"
-                  aspect-ratio="1"
-                  class="bg-grey-lighten-2"
-                  cover>
-              </v-img>
-            </v-col>
-          </template>
-          </v-row>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  
-    
-    
-  </v-row>
-</template>
 
 <style>
 .wedding-title {
--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import App from './App.vue'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
 
 const vuetify = createVuetify({
   theme: {
